d6ff8cc1698ecc9e7452ca9539e0bfe3db4eca91
Freddy Ignacio Colque <ignaciocolque99@gmail.com>
2022-09-02 12:56:49 -0300

    diseño del mapa con emojis

--- game.js
+++ game.js
@@ -10,9 +10,9 @@ window.addEventListener("resize", setCanvasSize)
 function setCanvasSize() {
 
     if(window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
+        canvasSize = window.innerWidth * 0.7;
     }else {
-        canvasSize = window.innerHeight * 0.8;
+        canvasSize = window.innerHeight * 0.7;
     }
 
     canvas.setAttribute("widht", canvasSize);
@@ -28,8 +28,15 @@ function startGame() {
     game.font = elementsSize + "px Verdana";
     game.textAling = "end";
 
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementsSize, elementsSize * i);
+    const map = maps[0];
+    const mapRows = map.trim().split('\n')
+    const mapRowCols = mapRows.map(row => row.trim().split(''))
+
+    for (let row = 1; row <= 10; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+        }
+        //game.fillText(emojis['X'], elementsSize, elementsSize * i);
     }
 }
 
