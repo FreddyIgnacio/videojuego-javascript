751fe644bb446b1988517285e6cc07f49dbfbcae
Freddy Ignacio Colque <ignaciocolque99@gmail.com>
2022-09-02 13:19:26 -0300

    Reajuste al mapa del juego en JS

--- game.js
+++ game.js
@@ -10,9 +10,9 @@ window.addEventListener("resize", setCanvasSize)
 function setCanvasSize() {
 
     if(window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.7;
+        canvasSize = window.innerWidth * 0.8;
     }else {
-        canvasSize = window.innerHeight * 0.7;
+        canvasSize = window.innerHeight * 0.8;
     }
 
     canvas.setAttribute("widht", canvasSize);
@@ -26,18 +26,20 @@ function startGame() {
     console.log({ canvasSize, elementsSize });
 
     game.font = elementsSize + "px Verdana";
-    game.textAling = "end";
+    game.textAlign = 'end';
 
     const map = maps[0];
     const mapRows = map.trim().split('\n')
     const mapRowCols = mapRows.map(row => row.trim().split(''))
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-        }
-        //game.fillText(emojis['X'], elementsSize, elementsSize * i);
-    }
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emoji, posX, posY);
+        });
+    });
 }
 
     
